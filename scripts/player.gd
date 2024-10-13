8dcf8e708d9879bdc4e259dc6135c9355550bfd3
ae0nic <Benjamin.M.Conley@gmail.com>
2024-10-13 16:03:47 -0500

    Cleanup time

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 @onready var camera := $Camera3D
+@onready var ui := %UI
+
 @export_category("Controller")
 @export var speed : float
 @export var run_multiplier : float
@@ -10,6 +12,8 @@ extends CharacterBody3D
 @export var air_resistance_multiplier : float
 @export var air_movement_modifier : float
 @export var fov := 75
+
+
 
 func _unhandled_input(event):
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
@@ -31,6 +35,7 @@ func _physics_process(delta):
 		velocity.y += jump_velocity
 	
 	var input_direction : Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	
 	var move_direction : Vector3 = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
 	var movement_multiplier = speed
 	if (Input.is_action_pressed("move_run")):
@@ -43,6 +48,7 @@ func _physics_process(delta):
 	camera.fov = lerpf(fov, fov * run_multiplier, clamp(0, 1, run_time_elapsed * 2)) as int
 	
 	
+	
 	velocity += move_direction * speed * movement_multiplier
 	if is_on_floor():
 		velocity.x *= ground_friction_multiplier
@@ -51,5 +57,15 @@ func _physics_process(delta):
 		velocity.x *= air_resistance_multiplier
 		velocity.z *= air_resistance_multiplier
 	prev_pos = position
+	ui.set_property("Velocity", str(velocity.length()))
+	
+
+func _process(delta):
+	if Input.is_action_just_pressed("debug"):
+		if ui.is_debug_visible():
+			ui.hide_debug()
+		else:
+			ui.show_debug()
+
 
 	move_and_slide()
