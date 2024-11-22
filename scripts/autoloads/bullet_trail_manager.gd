04889e39a5b57b19a93f8248a9fcc716b0d7b21e
ae0nic <bzminecraft247@gmail.com>
2024-11-22 14:34:51 -0600

    Added animations and bounce fade delay to the bullet trail

--- scripts/autoloads/bullet_trail_manager.gd
+++ scripts/autoloads/bullet_trail_manager.gd
@@ -4,7 +4,7 @@ extends Node
 const scene : PackedScene = preload("res://scenes/bullet_tracer.tscn")
 const shader : ShaderMaterial = preload("res://assets/materials/bullet_trail.tres")
 
-func create_bullet_trail(start: Vector3, end: Vector3, direction: Vector3, width: float) -> MeshInstance3D:
+func create_bullet_trail(start: Vector3, end: Vector3, direction: Vector3, width: float, delay: float = 0) -> MeshInstance3D:
 	var mesh := scene.instantiate()
 	
 	mesh.custom_aabb = AABB(Vector3(-10000, -10000, -10000), Vector3(20000, 20000, 20000))
@@ -14,6 +14,9 @@ func create_bullet_trail(start: Vector3, end: Vector3, direction: Vector3, width
 	mesh.set_axis(-direction)
 	mesh.set_length(abs((end-start).length()))
 	mesh.set_width(width)
+	mesh.set_delay(delay)
+	mesh.set_mask_fade_speed(1. / 3000.)
+	mesh.set_animation_speed(10)
 	
 	mesh.transform.origin = start
 	mesh.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
