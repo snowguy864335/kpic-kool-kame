d3d11bbe6fa03aba8ce93c97b72135ca4e9361e9
ae0nic <Benjamin.M.Conley@gmail.com>
2024-12-01 12:20:33 -0600

    Tweaked spells

--- scripts/classes/wizard/spells/wind_dash.gd
+++ scripts/classes/wizard/spells/wind_dash.gd
@@ -1,9 +1,10 @@
 extends WizardSpell
 class_name WindDash
 
-var mana_cost : int = 10
-var cooldown : float = 2
-var dash_velocity : float = 20
+@export var dash_velocity : float = 50
+@export var mana_cost : int = 10
+@export var cooldown : float = 2
+
 
 func use(player : NodePath) -> bool:
 	var playerNode = Engine.get_main_loop().current_scene.get_node(player)
